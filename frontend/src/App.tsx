18208f61026104e585c055bd17574d858af2b75e
EriRan <erik.rantanen@hotmail.fi>
2022-05-01 13:44:58 +0300

    Add CityInput
    Will be used to provide parameter for restaurant response API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Provider } from 'react-redux'
 import { store } from './app/store'
+import { CityInput } from './components/CityInput'
 import { RestaurantView } from './components/RestaurantView'
 
 export function App() {
   return (
     <Provider store={store}>
+      <CityInput />
       <RestaurantView />
     </Provider>
   )
---
+++ frontend/src/components/CityInput.tsx
@@ -0,0 +1,10 @@
+export function CityInput() {
+  return (
+    <div>
+      <input id="city-input" placeholder="Kaupungin nimi..." />
+      <button type="button" id="city-input-button">
+        Hae
+      </button>
+    </div>
+  )
+}
