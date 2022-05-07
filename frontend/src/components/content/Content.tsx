270ba60239a1d4682a79c690fb453bfa2f41c8df
EriRan <erik.rantanen@hotmail.fi>
2022-05-07 15:09:04 +0300

    Remove unused imports

--- frontend/src/components/content/Content.tsx
+++ frontend/src/components/content/Content.tsx
@@ -1,5 +1,3 @@
-import { Box, Container } from '@mui/material'
-import { MATERIAL_UI_MARGIN } from '../Constants'
 import { RestaurantContent } from './restaurant/RestaurantContent'
 
 export function Content() {
