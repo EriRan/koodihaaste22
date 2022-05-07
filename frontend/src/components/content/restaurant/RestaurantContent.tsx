9880708f8a3a0f6fbf8442f8b410c73ea684f457
EriRan <erik.rantanen@hotmail.fi>
2022-05-07 14:18:49 +0300

    Add header

--- frontend/src/components/content/restaurant/RestaurantContent.tsx
+++ frontend/src/components/content/restaurant/RestaurantContent.tsx
@@ -1,9 +1,11 @@
+import { Typography } from '@mui/material'
 import { CityInput } from './CityInput'
 import { RestaurantView } from './RestaurantView'
 
 export function RestaurantContent() {
   return (
     <div>
+      <Typography variant="h3">Etsi lounasravintolat</Typography>
       <CityInput />
       <RestaurantView />
     </div>
