2f055c9d353f63d8ef6adea93019fa6a5fb58041
EriRan <erik.rantanen@hotmail.fi>
2022-05-07 14:33:04 +0300

    Add styling
    The lack of margins here is pissing me off

--- frontend/src/components/content/restaurant/RestaurantView.tsx
+++ frontend/src/components/content/restaurant/RestaurantView.tsx
@@ -1,17 +1,25 @@
-import { Box } from '@mui/material'
+import { Box, Card, CardContent, Grid, Typography } from '@mui/material'
 import { useAppSelector } from 'src/app/hooks'
 import { selectRestaurantResponse } from 'src/app/state/restaurant/restaurantSlice'
+import AccessTimeIcon from '@mui/icons-material/AccessTime'
 
 export function RestaurantView() {
   const restaurantResponse = useAppSelector(selectRestaurantResponse)
 
   const restaurantsJsx = restaurantResponse?.restaurants.map(restaurant => {
     return (
-      <div id={`restaurant-${restaurant.id}`}>
-        <p>Name: {restaurant.name}</p>
-        <p>Opening hours: {restaurant.openingHours}</p>
-        <p>Votes: {restaurant.votes}</p>
-      </div>
+      <Card id={`restaurant-${restaurant.id}`}>
+        <CardContent>
+          <Typography variant="h4" id={`restaurant-${restaurant.id}-name`}>
+            {restaurant.name}
+          </Typography>
+          <Grid container>
+            <AccessTimeIcon />
+            <Typography>{restaurant.openingHours.trim()}</Typography>
+          </Grid>
+          <Typography>Votes: {restaurant.votes}</Typography>
+        </CardContent>
+      </Card>
     )
   })
   return <Box>{restaurantsJsx}</Box>
