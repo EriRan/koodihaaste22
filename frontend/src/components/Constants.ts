a8bc188bcdd15b69150b56728731524f3dbbd43d
EriRan <erik.rantanen@hotmail.fi>
2022-05-07 13:47:56 +0300

    Add top padding
    Also a new class to house the margin value material ui had
    by default

---
+++ frontend/src/components/Constants.ts
@@ -0,0 +1,4 @@
+/**
+ * Amount of margin placed on <body> by default by Material UI. This had to be overridden in index.html so that the appbar would fill the whole upper area without gutters
+ */
+export const MATERIAL_UI_MARGIN = '8px'
--- frontend/src/components/content/Content.tsx
+++ frontend/src/components/content/Content.tsx
@@ -1,12 +1,15 @@
-import { Container } from '@mui/material'
+import { Box, Container } from '@mui/material'
+import { MATERIAL_UI_MARGIN } from '../Constants'
 import { CityInput } from './restaurant/CityInput'
 import { RestaurantView } from './restaurant/RestaurantView'
 
 export function Content() {
   return (
     <Container>
-      <CityInput />
-      <RestaurantView />
+      <Box sx={{ paddingTop: MATERIAL_UI_MARGIN }}>
+        <CityInput />
+        <RestaurantView />
+      </Box>
     </Container>
   )
 }
