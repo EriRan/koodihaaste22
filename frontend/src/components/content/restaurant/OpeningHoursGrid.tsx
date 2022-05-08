3bebf8c075524d6fae152c338fc51becdc21dedf
EriRan <erik.rantanen@hotmail.fi>
2022-05-08 12:22:03 +0300

    Gray out no lunch scenarios too

--- frontend/src/components/content/restaurant/OpeningHoursGrid.tsx
+++ frontend/src/components/content/restaurant/OpeningHoursGrid.tsx
@@ -1,6 +1,8 @@
 import { Grid, Typography } from '@mui/material'
 import AccessTimeIcon from '@mui/icons-material/AccessTime'
 
+const NO_LUNCH_AVAILABLE = 'ei lounasta'
+
 export function OpeningHoursGrid(props: { openingHours: string; id: string }) {
   const { openingHours, id } = props
   const trimmedString = openingHours.trim()
@@ -13,7 +15,14 @@ export function OpeningHoursGrid(props: { openingHours: string; id: string }) {
       </Grid>
     )
   }
-
+  if (trimmedString === NO_LUNCH_AVAILABLE) {
+    return (
+      <Grid id={id} container>
+        <AccessTimeIcon color="disabled" />
+        <Typography>Ei lounasta</Typography>
+      </Grid>
+    )
+  }
   return (
     <Grid container>
       <AccessTimeIcon />
