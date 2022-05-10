5c69c76a8200d23911f40aded3a19f1fae8359ab
EriRan <erik.rantanen@hotmail.fi>
2022-05-10 18:45:06 +0300

    Add simple getVotes API call
    This needs to be done with specialized handling like a poller and
    calling once during init

--- frontend/src/components/content/votes/VotesContent.tsx
+++ frontend/src/components/content/votes/VotesContent.tsx
@@ -1,3 +1,35 @@
+import { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from 'src/app/hooks'
+import { selectVoteResult } from 'src/app/state/vote/voteSlice'
+import { getCurrentDayVotes } from 'src/app/state/vote/voteThunk'
+
 export function VotesContent() {
-  return <div>Votes Content</div>
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(getCurrentDayVotes())
+  }, [])
+
+  const voteResult = useAppSelector(selectVoteResult)
+
+  if (!voteResult) {
+    return <div>No votes</div>
+  }
+
+  const votingResultsJsx = voteResult.results.map(restaurantVote => {
+    return (
+      <div>
+        <p>City: {restaurantVote.city}</p>
+        <p>RestaurantId: {restaurantVote.restaurantid}</p>
+        <p>Name: {restaurantVote.name}</p>
+        <p>Votes: {restaurantVote.votes}</p>
+      </div>
+    )
+  })
+  return (
+    <div>
+      <p>Date: {voteResult.date}</p>
+      {votingResultsJsx}
+    </div>
+  )
 }
