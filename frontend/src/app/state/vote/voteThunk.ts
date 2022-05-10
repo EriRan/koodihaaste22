3ba57f253deacaeb824513468f5ce3d174dbe945
EriRan <erik.rantanen@hotmail.fi>
2022-05-10 18:56:55 +0300

    Add initial vote post thunk
    Need to add cookie logic for this

--- frontend/src/app/state/vote/voteThunk.ts
+++ frontend/src/app/state/vote/voteThunk.ts
@@ -20,3 +20,21 @@ export const getCurrentDayVotes = createAsyncThunk<
       return thunkAPI.rejectWithValue({ error: castedError.message })
     })
 })
+
+export const postVote = createAsyncThunk<
+  void,
+  string,
+  {
+    state: RootState
+  }
+>('vote/postVote', async (restaurantId: string, thunkAPI) => {
+  return axios
+    .post(`http://localhost:8080/api/v1/vote/${restaurantId}`)
+    .then(apiResponse => {
+      console.log(apiResponse.status)
+    })
+    .catch(error => {
+      const castedError = error as Error | AxiosError
+      return thunkAPI.rejectWithValue({ error: castedError.message })
+    })
+})
